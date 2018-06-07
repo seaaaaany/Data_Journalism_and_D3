753c33b723790fb68d18ba8ee7a2eeec6f3cfd3f
Sean <yycyjqc@gmail.com>
2018-06-06 23:22:25 -0700

    flask

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-#!/usr/bin/env python
+from flask import Flask, render_template, jsonify
+import pandas as pd
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/data')
+def data():
+
+    df = pd.read_csv('data/data.csv')
+    return jsonify(df.to_dict(orient="records"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
